257d1f1686de21594b9a5395661fd145aeca645e
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-05-13 17:16:02 -0300

    Initial Commit ApplianceShop

---
+++ src/ApplianceShop/Appliance.h
@@ -0,0 +1,12 @@
+#ifndef TPCUAT_GRUPO11_APPLIANCE_H
+#define TPCUAT_GRUPO11_APPLIANCE_H
+
+typedef struct Appliance{
+    char* name;
+    int price;
+    int idAppliance;
+    int idProvider;
+    int idManufacturer;
+}Appliance;
+
+#endif //TPCUAT_GRUPO11_APPLIANCE_H
