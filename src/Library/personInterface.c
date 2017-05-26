dce1618f447226fc58d2f628d9ab5ef65c713c21
Manuel Pedrozo <manuel.pedrozo@ing.austral.edu.ar>
2017-05-26 13:36:14 -0300

    mao

--- src/Library/personInterface.c
+++ src/Library/personInterface.c
@@ -3,27 +3,109 @@
 #include "../Util/ScanUtil.h"
 
 void personListMaterialMenu(Library* library, Person* person){
-    if(person->amountOfBorrowedMaterial == 0){
-        printf("\nThere are no subjects for this career.\n");
+    if(library->amountOfMaterial == 0){
+        printf("\nThere are no materials.\n");
         return;
     }
-    printf("\n- Your borrowed materials -\n\n");
+    Material* materialArray[library->amountOfMaterial];
+    int k = 0;
+    printf("\n-- Books and Magazines --\n\n");
+    printf("\n- Books -\n\n");
+    for(int i = 0, j = 0; i<library->personMaxCapacity, j<library->amountOfBook;i++){
+        if(library->materialBooleanArray[i] != 0 ){
+            Material* material = library->materialArray[i];
+            if(material->type == 1){
+                j++;
+                materialArray[k] = material;
+                k++;
+                printf("%d. %s\n", j, material->title);
+                if(material->isTaken)
+                    printf("Available: No\n");
+                else
+                    printf("Available: Yes\n");
+            }
+        }
+    }
+    printf("\n- Magazines -\n\n");
+    for(int i = 0, j = 0; i<library->personMaxCapacity, j<library->amountOfMagazine;i++){
+        if(library->materialBooleanArray[i] != 0 ){
+            Material* material = library->materialArray[i];
+            if(material->type == 2){
+                j++;
+                materialArray[k] = material;
+                k++;
+                printf("%d. %s\n", j, material->title);
+                if(material->isTaken)
+                    printf("Available: No\n");
+                else
+                    printf("Available: Yes\n");
+
+            }
+
+        }
+    }
+    int material = 0;
+    while(1){
+        printf("\nInsert number of the material to borrow:\n");
+        printf("(-1 to exit)\n");
+        material = scanInt();
+        if(material == -1){
+            return;
+        }
+        if(material > 0 && material <= k){
+            if(materialArray[material-1]->isTaken){
+                printf("That material is already borrowed\n");
+            } else{
+                materialArray[material-1]->isTaken = 0;
+                addBorrowedMaterial(person, materialArray[material-1]->idMaterial);
+                printf("Material :%s: Borrowed.\n", materialArray[material-1]->title);
+            }
+        } else{
+            printf("Please insert a valid number.\n");
+        }
+    }
+}
+
+
+void personListBorrowedMaterialMenu(Library* library,Person* person){
+    if(person->amountOfBorrowedMaterial == 0){
+        printf("\nYou have no borrowed material.\n");
+        return;
+    }
+    Material* materialArray[person->amountOfBorrowedMaterial];
+    printf("\n-- Your borrowed material --\n\n");
     for(int i = 0, j = 0; i<person->borrowedMaterialMaxCapacity, j<person->amountOfBorrowedMaterial;i++){
         if(person->borrowedMaterial[i] != 0){
-            j++;
             Material* material = getMaterial(library, person->borrowedMaterial[i]);
+            materialArray[j] = material;
+            j++;
             printf("%d. %s\n", j, material->title);
-            //TODO
+        }
+    }
+    int material = 0;
+    while(1){
+        printf("\nInsert number of the material to return:\n");
+        printf("(-1 to exit)\n");
+        material = scanInt();
+        if(material == -1){
+            return;
+        }
+        if(material > 0 && material <= person->amountOfBorrowedMaterial){
+            materialArray[material-1]->isTaken = 0;
+            removeBorrowedMaterial(person, materialArray[material-1]->idMaterial);
+            printf("Material :%s: returned.\n", materialArray[material-1]->title);
+            return;
+        } else{
+            printf("Please insert a valid number.\n");
         }
     }
 }
 
 void personMenu(Library* library, Person* person){
     while(1) {
-        printf("-- Library --\n");
+        printf("\n-- Library --\n");
         printf("1. List Books and magazines\n");
         printf("2. My borrowed material\n");
-        printf("3. Return borrowed material\n");
         printf("-1. exit\n");
         int choice = scanInt();
         switch (choice) {
@@ -32,9 +114,6 @@ void personMenu(Library* library, Person* person){
                 break;
             case 2:
                 personListBorrowedMaterialMenu(library, person);
-                break;
-            case 3:
-                personListReturnMaterialMenu(library, person);
                 break;
             case -1:
                 return;
